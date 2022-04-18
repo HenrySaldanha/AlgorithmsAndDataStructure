3c353366c8528418ad57b39bc26c0d881cd3925e
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-04-18 00:25:24 -0300

    Add singly linked list

---
+++ Algorithms/src/DataStructure/List/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,123 @@
+﻿namespace Algorithms.DataStructure.List.SinglyLinkedList
+{
+    public class SinglyLinkedList<T>
+    {
+        public SinglyNode<T> FirstNode { get; set; }
+        public int Size { get; set; }
+
+        public SinglyLinkedList<T> Add(SinglyNode<T> node)
+        {
+            if (node is null)
+                return this;
+
+            node.NextNode = null;
+
+            if (FirstNode is null)
+            {
+                FirstNode = node;
+                Size++;
+                return this;
+            }
+
+            var temp = FirstNode;
+
+            while (temp.NextNode is not null)
+                temp = temp.NextNode;
+
+            temp.NextNode = node;
+
+            Size++;
+            return this;
+        }
+
+        public SinglyNode<T> Search(int? key = null)
+        {
+            if (FirstNode is null)
+                return null;
+
+            var temp = FirstNode;
+
+            for (var i = 0; i < Size; i++)
+            {
+                if (temp.Key.Equals(key))
+                    return temp;
+
+                temp = temp.NextNode;
+            }
+
+            return null;
+        }
+
+        public SinglyLinkedList<T> Remove(int? key = null)
+        {
+            if (FirstNode is null)
+                return this;
+
+            if (FirstNode.Key.Equals(key))
+            {
+                FirstNode = FirstNode.NextNode;
+                Size--;
+                return this;
+            }
+
+            var temp = FirstNode;
+            var i = 0;
+            while (temp.NextNode is not null && !temp.NextNode.Key.Equals(key) && i < Size)
+            {
+                temp = temp.NextNode;
+                i++;
+            }
+
+            if (i >= Size || temp.NextNode is null)
+                return this;
+
+            temp.NextNode = temp.NextNode.NextNode;
+
+            Size--;
+            return this;
+        }
+
+        public SinglyLinkedList<T> RemoveAt(int index)
+        {
+            if (FirstNode is null || index > Size - 1)
+                return this;
+
+            if (index == 0)
+            {
+                FirstNode = FirstNode.NextNode;
+                Size--;
+                return this;
+            }
+
+            var temp = FirstNode;
+            var i = 0;
+            while (i < Size)
+            {
+                if (i == index - 1)
+                    i = Size;
+                else
+                    temp = temp.NextNode;
+
+            }
+
+            temp.NextNode = temp.NextNode.NextNode;
+
+            Size--;
+            return this;
+        }
+
+        public void Traverse()
+        {
+            if (FirstNode is null) return;
+
+            var temp = FirstNode;
+
+            while (temp.NextNode is not null)
+            {
+                Console.Write(temp.Key + " -> ");
+                temp = temp.NextNode;
+            }
+            Console.WriteLine(temp.Key);
+        }
+    }
+}
---
+++ Algorithms/src/DataStructure/List/SinglyLinkedList/SinglyNode.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.DataStructure.List.SinglyLinkedList
+{
+    public class SinglyNode<T>
+    {
+        public SinglyNode(T key) => Key = key;
+
+        public T Key { get; set; }
+        public SinglyNode<T> NextNode { get; set; }
+    }
+}
