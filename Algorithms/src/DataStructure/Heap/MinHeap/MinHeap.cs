f14c22e16539d7722eccb7700e736610a20f418b
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-21 16:04:46 -0300

    Add MinHeap

---
+++ Algorithms/src/DataStructure/Heap/MinHeap/MinHeap.cs
@@ -0,0 +1,40 @@
+﻿namespace DataStructure.Heap.MinHeap
+{
+    public class MinHeap
+    {
+        public int Capacity { get; private set; }
+        public int?[] Heap { get; private set; }
+
+        public int Size { get; private set; }
+
+        public MinHeap(int capacity)
+        {
+            Capacity = capacity;
+            Heap = new int?[capacity];
+            Size = 0;
+        }
+
+        public MinHeap Add(int item)
+        {
+            if (Size == Capacity)
+                return this;
+
+            Heap[Size] = item;
+
+            for (int i = Size; i >= 0 && Heap[i] < Heap[ParentIndex(i)]; i--)
+            {
+                var temp = Heap[i];
+                Heap[i] = Heap[ParentIndex(i)];
+                Heap[ParentIndex(i)] = temp;
+
+                i = ParentIndex(i);
+
+            }
+
+            Size++;
+            return this;
+        }
+
+        private int ParentIndex(int key) => (key - 1) / 2;
+    }
+}
---
+++ Algorithms/test/DataStructure.Test/Heap/MinHeap/MinHeapTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace DataStructure.Test.Heap.MinHeap
+{
+    public class MinHeapTests
+    {
+        [Fact]
+        public void Add_ValidKey_MustAdd()
+        {
+            //Arrange
+            var heap = new DataStructure.Heap.MinHeap.MinHeap(3);
+
+            //Act
+            heap.Add(3).Add(8).Add(1);
+
+            //Assert
+            Assert.Equal(3, heap.Size);
+            Assert.Equal(1, heap.Heap[0]);
+            Assert.Equal(8, heap.Heap[1]);
+            Assert.Equal(3, heap.Heap[2]);
+        }
+
+        [Fact]
+        public void Add_Oversize_DontAdd()
+        {
+            //Arrange
+            var heap = new DataStructure.Heap.MinHeap.MinHeap(3);
+
+            //Act
+            heap.Add(3).Add(8).Add(1).Add(7);
+
+            //Assert
+            Assert.Equal(3, heap.Size);
+            Assert.Equal(1, heap.Heap[0]);
+            Assert.Equal(8, heap.Heap[1]);
+            Assert.Equal(3, heap.Heap[2]);
+        }
+    }
+}
