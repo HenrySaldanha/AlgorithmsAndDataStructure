0f19d2c46c095eed69f13894aa9c2f3dc4c0a332
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-23 17:10:44 -0300

    Add problem two sum

---
+++ Algorithms/Problems.Test/TwoSumTests.cs
@@ -0,0 +1,47 @@
+using Xunit;
+
+namespace Problems.Test
+{
+    public class TwoSumTests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9)]
+        [InlineData(new[] { 5, 75, 25 }, 100)]
+        [InlineData(new[] { 0, 3, -3, 4, -1 }, -1)]
+        [InlineData(new[] { 3, 2, 4 }, 6)]
+        [InlineData(new[] { 3, 3 }, 6)]
+        [InlineData(new[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(int[] array, int target)
+        {
+            //Arrange
+            var solution = new TwoSum();
+
+            //Act
+            var response = solution.SolutionA(array, target);
+
+            //Assert
+            Assert.Equal(array[response[0]] + array[response[1]], target);
+            Assert.True(response[0] < response[1]);
+        }
+
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9)]
+        [InlineData(new[] { 5, 75, 25 }, 100)]
+        [InlineData(new[] { 0, 3, -3, 4, -1 }, -1)]
+        [InlineData(new[] { 3, 2, 4 }, 6)]
+        [InlineData(new[] { 3, 3 }, 6)]
+        [InlineData(new[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11)]
+        public void SolutionB_ValidInput_MustReturnCorrectAnswer(int[] array, int target)
+        {
+            //Arrange
+            var solution = new TwoSum();
+
+            //Act
+            var response = solution.SolutionB(array, target);
+
+            //Assert
+            Assert.Equal(array[response[0]] + array[response[1]], target);
+            Assert.True(response[0] < response[1]);
+        }
+    }
+}
---
+++ Algorithms/src/Problems/TwoSum.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace Problems
+{
+
+    ///<summary>
+    /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    /// You can return the answer in any order.
+    ///</summary>
+
+    ///<example>
+    ///Input: nums = [2,7,11,15], target = 9
+    ///Output: [0,1]
+    ///</example>
+
+    ///<example>
+    ///Input: nums = [3,3], target = 6
+    ///Output: [0,1]
+    ///</example>
+
+    public class TwoSum
+    {
+        public int[] SolutionA(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (dict.TryGetValue(complement, out var res) && res != i)
+                {
+                    return i < res ? new[] { i, res } : new[] { res, i };
+                }
+
+                dict[nums[i]] = i;
+
+            }
+            return new int[] { -1, -1 };
+        }
+
+        public int[] SolutionB(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 0; j < nums.Length; j++)
+                {
+                    if (j == i)
+                        continue;
+
+                    if (nums[i] + nums[j] == target)
+                        return new[] { i, j };
+
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+
+
+    }
+}
