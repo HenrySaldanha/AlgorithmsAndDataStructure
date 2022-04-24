a227dfe352b9de377f2c9346340e104ae8ff0696
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-24 18:52:36 -0300

    Add new problem: Add Two Numbers

---
+++ Algorithms/src/Problems/AddTwoNumbers.cs
@@ -0,0 +1,77 @@
+﻿namespace Problems
+{
+
+    ///<summary>
+    /// You are given two non-empty linked lists representing two non-negative integers. 
+    /// The digits are stored in reverse order, and each of their nodes contains a single digit. 
+    /// Add the two numbers and return the sum as a linked list.
+    /// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    ///</summary>
+
+    ///<example>
+    /// Input: l1 = [2,4,3], l2 = [5,6,4]
+    /// Output: [7,0,8]
+    /// Explanation: 342 + 465 = 807.
+    ///</example>
+
+
+
+    public class AddTwoNumbers
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode SolutionA(ListNode l1, ListNode l2)
+        {
+            ListNode result = null;
+            ListNode pointerResult = null;
+            var carry = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                var sum = 0;
+
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                sum += carry;
+                carry = sum / 10;
+
+                if (result == null)
+                {
+                    result = new ListNode(sum >= 10 ? sum - 10 : sum);
+                    pointerResult = result;
+                }
+                else
+                {
+                    pointerResult.next = new ListNode(sum >= 10 ? sum - 10 : sum);
+                    pointerResult = pointerResult.next;
+                }
+            }
+
+            if (carry > 0)
+                pointerResult.next = new ListNode(carry);
+
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms/test/Problems.Test/AddTwoNumbersTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Problems.Test
+{
+    public class AddTwoNumbersTests
+    {
+        [Fact]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new AddTwoNumbers();
+            var nodeA2 = new AddTwoNumbers.ListNode(3);
+            var nodeA1 = new AddTwoNumbers.ListNode(4, nodeA2);
+            var rootA = new AddTwoNumbers.ListNode(2, nodeA1);
+
+            var nodeB2 = new AddTwoNumbers.ListNode(4);
+            var nodeB1 = new AddTwoNumbers.ListNode(6, nodeB2);
+            var rootB = new AddTwoNumbers.ListNode(5, nodeB1);
+
+            //Act
+            var response = solution.SolutionA(rootA, rootB);
+
+            //Assert
+            Assert.Equal(7, response.val);
+            Assert.Equal(0, response.next.val);
+            Assert.Equal(8, response.next.next.val);
+        }
+    }
+}
