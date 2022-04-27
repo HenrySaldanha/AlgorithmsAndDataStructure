0e8d653aaa37a1fa92a6bf63426f0ec877b73190
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-26 22:14:37 -0300

    Add new problem Greatest Common Divisor

---
+++ src/Problems/GreatestCommonDivisor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+
+    /// <summary>
+    /// Greatest Common Divisor: It is the highest number that completely divides two or more numbers. 
+    /// It is abbreviated for GCD. It is also known as the Greatest Common Factor (GCF) and the Highest Common Factor (HCF). 
+    /// It is used to simplify the fractions.
+    /// </summary>
+
+    /// <example>
+    /// Input: 9,6
+    /// Output: 3
+    /// Explanation: 3*2 = 6 
+    /// Explanation: 3*3 = 9 
+    /// </example>
+
+    public class GreatestCommonDivisor
+    {
+        public object SolutionA(int a, int b)
+        {
+            if (b == 0)
+                return a;
+
+            return SolutionA(b, a % b);
+        }
+    }
+}
---
+++ test/Problems.Test/GreatestCommonDivisorTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Problems.Test
+{
+    public class GreatestCommonDivisorTests
+    {
+        [Theory]
+        [InlineData(6,9, 3)]
+        [InlineData(7, 4,1)]
+        [InlineData(12,36,12)]
+        [InlineData(20,15,5)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(int a, int b, int answer)
+        {
+            //Arrange
+            var solution = new GreatestCommonDivisor();
+
+            //Act
+            var response = solution.SolutionA(a,b);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+    }
+}
