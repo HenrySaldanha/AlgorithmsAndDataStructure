f02f62d5b2d91450af91f6f28e64922a665ac59d
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-27 01:39:27 -0300

    Add new problem Median of Two Sorted Arrays

---
+++ src/Problems/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,54 @@
+﻿namespace Problems
+{
+    public class MedianOfTwoSortedArrays
+    {
+        public double SolutionA(int[] nums1, int[] nums2)
+        {
+            var index1 = 0;
+            var index2 = 0;
+
+            var current_value = 0.0;
+
+            if (((nums1.Length + nums2.Length) % 2 != 0))
+            {
+                var middle = (nums1.Length + nums2.Length - 1) / 2;
+
+                for (var i = 0; i <= middle; i++)
+                {
+                    if (index1 < nums1.Length && index2 < nums2.Length)
+                        current_value = (nums1[index1] <= nums2[index2]) ? nums1[index1++] : nums2[index2++];
+
+                    else if (index1 < nums1.Length)
+                        current_value = nums1[index1++];
+
+                    else if (index2 < nums2.Length)
+                        current_value = nums2[index2++];
+                }
+                return current_value;
+            }
+            else
+            {
+                var middle1 = ((nums1.Length + nums2.Length) / 2) - 1;
+                var middle2 = middle1 + 1;
+
+                var prev_value = 0.0;
+
+                for (var i = 0; i <= middle2; i++)
+                {
+                    prev_value = current_value;
+
+                    if (index1 < nums1.Length && index2 < nums2.Length)
+                        current_value = (nums1[index1] <= nums2[index2]) ? nums1[index1++] : nums2[index2++];
+
+                    else if (index1 < nums1.Length)
+                        current_value = nums1[index1++];
+
+                    else if (index2 < nums2.Length)
+                        current_value = nums2[index2++];
+                }
+
+                return (prev_value + current_value) / 2.0;
+            }
+        }
+    }
+}
---
+++ test/Problems.Test/MedianOfTwoSortedArraysTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace Problems.Test
+{
+    public class MedianOfTwoSortedArraysTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 3 }, new[] { 2 }, 2.0)]
+        [InlineData(new[] { 1, 2 }, new[] { 3, 4 }, 2.5)]
+        [InlineData(new[] { 0, 0 }, new[] { 0, 0 }, 0)]
+        [InlineData(null, new[] { 1 }, 1)]
+        [InlineData(new int[] { 0, 0, 0, 0, 0 }, new[] { -1, 0, 0, 0, 0, 0, 1 }, 0)]
+        [InlineData(null, new[] { 2, 3 }, 2.5)]
+        [InlineData(new[] { 2, 2, 4, 4 }, new[] { 2, 2, 4, 4 }, 3)]
+        [InlineData(new[] { 1, 1 }, new[] { 1, 2 }, 1)]
+        [InlineData(new[] { 1, 2 }, new[] { -1, 3 }, 1.5)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(int[] array1, int[] array2, double answer)
+        {
+            //Arrange
+            var solution = new MedianOfTwoSortedArrays();
+
+            //Act
+            var response = solution.SolutionA(array1 ?? Array.Empty<int>(), array2);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+    }
+}
