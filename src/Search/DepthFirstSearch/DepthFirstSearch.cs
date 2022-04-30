7e2b3097432043fae02bcdc3f5e1813d29bd8ae4
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-30 16:48:54 -0300

    Add Depth First Search

---
+++ src/Search/DepthFirstSearch/DepthFirstSearch.cs
@@ -0,0 +1,29 @@
+﻿using DataStructure.Graph;
+
+namespace Search.DepthFirstSearch
+{
+    public class DepthFirstSearch 
+    {
+        public AdjacencyListWeightless GraphStructure { get; private set; }
+        public bool[] Visited { get; set; }
+
+        public DepthFirstSearch(AdjacencyListWeightless graph)
+        {
+            GraphStructure = graph;
+            Visited = new bool[GraphStructure.Graph.Length];
+        }
+
+        public string Dfs(int origin)
+        {
+            var r = origin.ToString();
+            Visited[origin] = true;
+
+            foreach (var item in GraphStructure.Graph[origin])
+            {
+                if (!Visited[item])
+                    r += Dfs(item);
+            }
+            return r;
+        }
+    }
+}
---
+++ test/Search.Test/DepthFirstSearch/DepthFirstSearchTests.cs
@@ -0,0 +1,23 @@
+﻿using DataStructure.Graph;
+using Xunit;
+
+namespace Search.Test.DepthFirstSearch
+{
+    public class DepthFirstSearchTests
+    {
+        [Fact]
+        public void DepthFirstSearch_ValidEdge_MustReturnSuccess()
+        {
+            //Arrange
+            var graph = new AdjacencyListWeightless(8);
+            graph.AddEdge(1, 2).AddEdge(1, 3).AddEdge(2, 3).AddEdge(2, 4).AddEdge(4, 5).AddEdge(5, 7);
+            var search = new Search.DepthFirstSearch.DepthFirstSearch(graph);
+
+            //Act
+            var result = search.Dfs(1);
+
+            //Assert
+            Assert.Equal("123457", result);
+        }
+    }
+}
