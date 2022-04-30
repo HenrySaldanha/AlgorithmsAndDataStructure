4df9e23f41cf58c7bc196b95391896579b6e1610
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-30 18:53:27 -0300

    Add new problem

---
+++ src/Problems/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,57 @@
+﻿namespace Problems
+{
+    /// <summary>
+    /// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that 
+    /// each unique element appears only once. The relative order of the elements should be kept the same.
+    /// </summary>
+
+    /// <example>
+    /// Input: nums = [1,1,2]
+    /// Output: 2, nums = [1,2,_]
+    /// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+    /// </example>
+   
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int SolutionA(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return nums.Length;
+
+            var k = nums.Length;
+
+            for (var i = 1; i < k; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    k--;
+                    for (int j = i; j < k; j++)
+                        nums[j] = nums[j + 1];
+
+                    i--;
+                }
+            }
+
+            return k;
+        }
+
+        public int SolutionB(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return nums.Length;
+
+            var x = 1;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[x - 1])
+                {
+                    nums[x] = nums[i];
+                    x++;
+                }
+            }
+
+            return x;
+        }
+    }
+}
---
+++ test/Problems.Test/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Problems.Test
+{
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 3 }, 2)]
+        [InlineData(new[] { 1 }, 1)]
+        [InlineData(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(int[] numbers, int answer)
+        {
+            //Arrange
+            var solution = new RemoveDuplicatesFromSortedArray();
+
+            //Act
+            var response = solution.SolutionA(numbers);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 1, 3 }, 2)]
+        [InlineData(new[] { 1 }, 1)]
+        [InlineData(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void SolutionB_ValidInput_MustReturnCorrectAnswer(int[] numbers, int answer)
+        {
+            //Arrange
+            var solution = new RemoveDuplicatesFromSortedArray();
+
+            //Act
+            var response = solution.SolutionB(numbers);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+
+    }
+}
