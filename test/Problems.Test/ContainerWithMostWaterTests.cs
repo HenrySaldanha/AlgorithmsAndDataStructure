e9be2ed973a4456ea77e49c07ca3670471efb5bb
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-04-30 22:18:16 -0300

    Add new problem: Container With Most Water

---
+++ test/Problems.Test/ContainerWithMostWaterTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Problems.Test
+{
+    public class ContainerWithMostWaterTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new[] { 1, 1 }, 1)]
+        [InlineData(new[] { 1,1,2,3,4 }, 4)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(int[] heights, int answer)
+        {
+            //Arrange
+            var solution = new ContainerWithMostWater();
+
+            //Act
+            var response = solution.SolutionA(heights);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+    }
+}
