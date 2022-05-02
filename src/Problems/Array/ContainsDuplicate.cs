ddd232af5efdf3f4d4b8d513b7c4ea353e05e99c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-02 19:29:34 -0300

    Add new problem: ContainsDuplicate

---
+++ src/Problems/Array/ContainsDuplicate.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems.Array
+{
+    /// <summary>
+    /// Given an integer array nums, return true if any value appears at least twice in the array,
+    /// and return false if every element is distinct.
+    /// </summary>
+
+    /// <example>
+    /// Input: nums = [1,2,3,1]
+    /// Output: true
+    /// </example>
+
+    /// <example>
+    /// Input: nums = [1,2,3,4]
+    /// Output: false
+    /// </example>
+
+    public class ContainsDuplicate
+    {
+        public bool SolutionA(int[] nums)
+        {
+            var frequency = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (frequency.Contains(nums[i]))
+                    return true;
+
+                frequency.Add(nums[i]);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/Problems.Test/Array/ContainsDuplicateTests.cs
@@ -0,0 +1,23 @@
+﻿using Problems.Array;
+using Xunit;
+
+namespace Problems.Test.Array
+{
+    public class ContainsDuplicateTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 1 }, true)]
+        [InlineData(new[] { 1, 2, 3, 4 }, false)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(int[] numbers, bool answer)
+        {
+            //Arrange
+            var solution = new ContainsDuplicate();
+
+            //Act
+            var response = solution.SolutionA(numbers);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+    }
+}
