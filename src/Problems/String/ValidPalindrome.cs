6deec788ef5188a3f6e2ed01943187f8a13486bc
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-02 22:31:12 -0300

    Add new Problem: Valid Palindrome

---
+++ src/Problems/String/ValidPalindrome.cs
@@ -0,0 +1,56 @@
+﻿namespace Problems.String
+{
+    /// <summary>
+    /// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing 
+    /// all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+    /// Given a string s, return true if it is a palindrome, or false otherwise.
+    /// </summary>
+
+    /// <example>
+    /// Input: s = "A man, a plan, a canal: Panama"
+    /// Output: true
+    /// </example>
+
+    /// <example>
+    /// Input: s = "race a car"
+    /// Output: false
+    /// </example>
+
+    public class ValidPalindrome
+    {
+        public bool SolutionA(string s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (IsLetterOrNumber(s[left]) && IsLetterOrNumber(s[right]))
+                {
+                    if (ToUpper(s[left]) != ToUpper(s[right]))
+                        return false;
+
+                    left++;
+                    right--;
+                }
+                else
+                {
+                    if (!IsLetterOrNumber(s[left]))
+                        left++;
+
+                    if (!IsLetterOrNumber(s[right]))
+                        right--;
+                }
+            }
+
+            return true;
+        }
+        private bool IsLetterOrNumber(char input) =>
+            input >= 'a' && input <= 'z' || 
+            input >= 'A' && input <= 'Z' ||
+            input >= '0' && input <= '9';
+
+        private char ToUpper(char input) =>
+            input >= 'a' && input <= 'z' ? ((char)(input - 32)) : input;
+    }
+}
---
+++ test/Problems.Test/String/ValidPalindromeTests.cs
@@ -0,0 +1,23 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class ValidPalindromeTests
+    {
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(string input, bool responseExpected)
+        {
+            //Arrange
+            var solution = new ValidPalindrome();
+
+            //Act
+            var response = solution.SolutionA(input);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
