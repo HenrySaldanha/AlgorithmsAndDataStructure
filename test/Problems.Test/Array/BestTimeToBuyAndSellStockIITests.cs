f8df115f5ced952eb8f9287f7d21245a15b1aba2
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-07 18:21:30 -0300

    Add new problem: Rotate Array

--- test/Problems.Test/Array/BestTimeToBuyAndSellStockIITests.cs
+++ test/Problems.Test/Array/BestTimeToBuyAndSellStockIITests.cs
@@ -6,7 +6,7 @@ namespace Problems.Test.Array
     public class BestTimeToBuyAndSellStockIITests
     {
         [Theory]
-        [InlineData(new[] {7, 1, 5, 3, 6, 4 }, 7)]
+        [InlineData(new[] { 7, 1, 5, 3, 6, 4 }, 7)]
         [InlineData(new[] { 1, 2, 3, 4, 5 }, 4)]
         public void Solution_ValidInput_MustReturnCorrectAnswer(int[] heights, int answer)
         {
