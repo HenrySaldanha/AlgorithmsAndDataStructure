131ddbefcdfb667624ec960c28e350bfa292f1d0
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-07 16:22:16 -0300

    Add new problem: Best Time To Buy And Sell Stock II

---
+++ test/Problems.Test/Array/BestTimeToBuyAndSellStockIITests.cs
@@ -0,0 +1,24 @@
+﻿using Problems.Array;
+using Xunit;
+
+namespace Problems.Test.Array
+{
+    public class BestTimeToBuyAndSellStockIITests
+    {
+        [Theory]
+        [InlineData(new[] {7, 1, 5, 3, 6, 4 }, 7)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 4)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(int[] heights, int answer)
+        {
+            //Arrange
+            var solution = new BestTimeToBuyAndSellStockII();
+
+            //Act
+            var response = solution.Solution(heights);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+    }
+}
+
