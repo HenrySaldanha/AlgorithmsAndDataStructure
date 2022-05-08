f95cce95648c8820c69713ed823a9affdc5a4551
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-08 12:41:57 -0300

    Add new problem:

---
+++ test/Problems.Test/String/LargestThreeSameDigitNumberInStringTests.cs
@@ -0,0 +1,24 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class LargestThreeSameDigitNumberInStringTests
+    {
+        [Theory]
+        [InlineData("6777133339", "777")]
+        [InlineData("2300019", "000")]
+        [InlineData("42352338", "")]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(string input, string responseExpected)
+        {
+            //Arrange
+            var solution = new LargestThreeSameDigitNumberInString();
+
+            //Act
+            var response = solution.Solution(input);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
