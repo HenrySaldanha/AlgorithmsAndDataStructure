9c08113a98c960978a3195f643dfc24fa294b3d2
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-07 23:11:14 -0300

    Add new problem: Reverse String

---
+++ src/Problems/String/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿namespace Problems.String
+{
+    /// <summary>
+    /// Write a function that reverses a string. The input string is given as an array of characters s.
+    /// You must do this by modifying the input array in-place with O(1) extra memory.
+    /// </summary>
+
+    /// <example>
+    /// Input: s = ["h","e","l","l","o"]
+    /// Output: ["o","l","l","e","h"]
+    /// </example>
+
+    public class ReverseString
+    {
+        public void Solution(char[] s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left < right)
+            {
+                (s[left], s[right]) = (s[right], s[left]);
+                left++;
+                right--;
+            }
+        }
+    }
+}
---
+++ test/Problems.Test/String/ReverseStringTests.cs
@@ -0,0 +1,23 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class ReverseStringTests
+    {
+        [Theory]
+        [InlineData(new[] { 'h', 'e', 'l', 'l', 'o' }, new[] { 'o', 'l', 'l', 'e', 'h' })]
+        [InlineData(new[] { 'a', 'b', 'c' }, new[] { 'c', 'b', 'a' })]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(char[] input, char[] responseExpected)
+        {
+            //Arrange
+            var solution = new ReverseString();
+
+            //Act
+            solution.Solution(input);
+
+            //Assert
+            Assert.Equal(responseExpected, input);
+        }
+    }
+}
