10ae3897143590a4446cde80f87734748941f81f
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-08 12:57:30 -0300

    Add new problem: Swap nodes in pairs

---
+++ src/Problems/LinkedList/SwapNodesInPairs.cs
@@ -0,0 +1,51 @@
+﻿namespace Problems.LinkedList
+{
+    /// <summary>
+    /// Given a linked list, swap every two adjacent nodes and return its head. 
+    /// You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+    /// </summary>
+
+    /// <example>
+    /// Input: head = [1,2,3,4]
+    /// Output: [2,1,4,3]
+    /// </example>
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class SwapNodesInPairs
+    {
+        public ListNode Solution(ListNode head)
+        {
+            if(head?.next is null)
+                return head;
+
+            var newHead = new ListNode();
+            newHead.next = head;
+            var prev = newHead;
+
+            while(prev.next is not null && prev.next.next is not null)
+            {
+                var first = prev.next;
+                var second = prev.next.next;
+
+                first.next = second.next;
+                second.next = first;
+
+                prev.next = second;
+
+                prev = first;
+            }
+
+            return newHead.next;
+        }
+    }
+}
---
+++ test/Problems.Test/LinkedList/SwapNodesInPairsTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.LinkedList;
+using Xunit;
+
+namespace Problems.Test.LinkedList
+{
+    public class SwapNodesInPairsTests
+    {
+        [Fact]
+        public void Solution_ValidInput_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new SwapNodesInPairs();
+            var head = new ListNode(1, new ListNode(2,new ListNode(3, new ListNode(4))));
+
+            //Act
+            var response = solution.Solution(head);
+
+            //Assert
+            Assert.Equal(2, response.val);
+            Assert.Equal(1, response.next.val);
+            Assert.Equal(4, response.next.next.val);
+            Assert.Equal(3, response.next.next.next.val);
+        }
+    }
+}
