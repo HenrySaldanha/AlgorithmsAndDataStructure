b438b6572fa3bceaf2726c7bf9776b587510937a
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-08 17:46:06 -0300

    Add new Problem: Partition Equal Subset Sum

---
+++ src/Problems/Array/PartitionEqualSubsetSum.cs
@@ -0,0 +1,31 @@
+﻿namespace Problems.Array
+{
+    public class PartitionEqualSubsetSum
+    {
+        public bool Solution(int[] nums)
+        {
+            var totalSum = nums.Sum();
+            if(totalSum % 2 != 0) return false;
+
+            return HasSumSubGroup(nums, 0, 0, totalSum, new Dictionary<string, bool>());
+        }
+
+        public bool HasSumSubGroup(int[] nums, int index, int sum, int total, Dictionary<string, bool> dict)
+        {
+            if (dict.ContainsKey($"{index}{sum}"))
+                return dict[$"{index}{sum}"];
+
+            if (sum * 2 == total)
+                return true;
+
+            if(sum*2 > total || index >= nums.Length) 
+                return false;
+
+            var hasSum = HasSumSubGroup(nums, index+1, sum + nums[index], total, dict) || HasSumSubGroup(nums, index + 1, sum , total, dict);
+
+            dict[$"{index}{sum}"] = hasSum;
+
+            return hasSum;
+        }
+    }
+}
