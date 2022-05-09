6a6ca922429d75b433b7a4db7ece135bfef60ac0
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-08 23:15:22 -0300

    Add new problem: Remove Nth Node From End of List

---
+++ test/Problems.Test/LinkedList/RemoveNthNodeFromEndOfListTests.cs
@@ -0,0 +1,45 @@
+﻿using Problems.LinkedList;
+using Xunit;
+using static Problems.LinkedList.RemoveNthNodeFromEndOfList;
+
+namespace Problems.Test.LinkedList
+{
+    public class RemoveNthNodeFromEndOfListTests
+    {
+        [Fact]
+        public void Solution_RemoveLast_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new RemoveNthNodeFromEndOfList();
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+
+            //Act
+            var result = solution.Solution(head, 1);
+
+            //Assert
+            Assert.Equal(1, result.val);
+            Assert.Equal(2, result.next.val);
+            Assert.Null(result.next.next);
+        }
+
+        [Fact]
+        public void Solution_RemoveFirst_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new RemoveNthNodeFromEndOfList();
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+
+            //Act
+            var result = solution.Solution(head, 3);
+
+            //Assert
+            Assert.Equal(2, result.val);
+            Assert.Equal(3, result.next.val);
+            Assert.Null(result.next.next);
+        }
+    }
+}
