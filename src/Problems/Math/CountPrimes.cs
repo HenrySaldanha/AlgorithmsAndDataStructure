f046dd0a8d586be1bee9f4e59a9929a5d1cc9462
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-05-11 20:08:32 -0300

    Add new problem Count Primes

---
+++ src/Problems/Math/CountPrimes.cs
@@ -0,0 +1,40 @@
+﻿namespace Problems.Math
+{
+    /// <summary>
+    /// Given an integer n, return the number of prime numbers that are strictly less than n.
+    /// </summary>
+
+    /// <example>
+    /// Input: n = 10
+    /// Output: 4
+    /// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+    /// </example>
+    
+    public class CountPrimes
+    {
+        public int Solution(int n)
+        {
+            var primes = new bool[n];
+            var count = 0;
+
+            for (int i = 0; i < primes.Length; i++)
+                primes[i] = true;
+
+            for (int i = 2; i * i < primes.Length; i++)
+            {
+                if (primes[i])
+                {
+                    for (int j = i; j * i < primes.Length; j++)
+                        primes[i * j] = false;
+                }
+            }
+
+            for (int i = 2; i < primes.Length; i++)
+            {
+                if (primes[i])
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ test/Problems.Test/Math/CountPrimesTests.cs
@@ -0,0 +1,24 @@
+﻿using Problems.Math;
+using Xunit;
+
+namespace Problems.Test.Math
+{
+    public class CountPrimesTests
+    {
+        [Theory]
+        [InlineData(10, 4)]
+        [InlineData(0, 0)]
+        [InlineData(1, 0)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(int n, int answer)
+        {
+            //Arrange
+            var solution = new CountPrimes();
+
+            //Act
+            var response = solution.Solution(n);
+
+            //Assert
+            Assert.Equal(answer, response);
+        }
+    }
+}
