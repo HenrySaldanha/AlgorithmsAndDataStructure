7f70094de04a640240521e67802e64bfc5685971
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-10 23:15:25 -0300

    Add new problem: Binary Tree Inorder Traversal

---
+++ test/Problems.Test/DepthFirstSearch/BinaryTreeInorderTraversalTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.DepthFirstSearch;
+using Xunit;
+
+namespace Problems.Test.DepthFirstSearch
+{
+    public class BinaryTreeInorderTraversalTests
+    {
+        [Fact]
+        public void Solution_ValidInput_MustReturnTrue()
+        {
+            //Arrange
+            var solution = new BinaryTreeInorderTraversal();
+            var tree = new BinaryTreeInorderTraversal.TreeNode(1, null,
+                new BinaryTreeInorderTraversal.TreeNode(2, new BinaryTreeInorderTraversal.TreeNode(3)));
+
+            //Act
+            var response = solution.Solution(tree);
+
+            //Assert
+            Assert.Equal(1, response[0]);
+            Assert.Equal(3, response[1]);
+            Assert.Equal(2, response[2]);
+        }
+    }
+}
