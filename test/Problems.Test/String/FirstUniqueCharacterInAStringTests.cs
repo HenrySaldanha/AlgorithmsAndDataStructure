beddd03948a9e1434bf2f428dac5ea5b8a37cf1c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-14 17:04:15 -0300

    Add new problem: First Unique Character in a String

---
+++ test/Problems.Test/String/FirstUniqueCharacterInAStringTests.cs
@@ -0,0 +1,24 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class FirstUniqueCharacterInAStringTests
+    {
+        [Theory]
+        [InlineData("leetcode", 0)]
+        [InlineData("loveleetcode", 2)]
+        [InlineData("aabb", -1)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(string input, int responseExpected)
+        {
+            //Arrange
+            var solution = new FirstUniqueCharacterInAString();
+
+            //Act
+            var response = solution.Solution(input);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
