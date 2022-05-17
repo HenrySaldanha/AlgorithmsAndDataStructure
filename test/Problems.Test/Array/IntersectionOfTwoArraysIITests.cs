ab81df316b0e8666a82b5ef381b153e6dfe013d2
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-17 20:54:39 -0300

    Add new problem: Intersection of Two Arrays II

---
+++ test/Problems.Test/Array/IntersectionOfTwoArraysIITests.cs
@@ -0,0 +1,38 @@
+﻿using Problems.Array;
+using Xunit;
+
+namespace Problems.Test.Array
+{
+    public class IntersectionOfTwoArraysIITests
+    {
+        [Fact]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new IntersectionOfTwoArraysII();
+
+            //Act
+            var response = solution.SolutionA(new[] { 1, 2, 3 }, new[] { 1, 2 });
+
+            //Assert
+            Assert.Equal(2, response.Length);
+            Assert.Equal(1, response[0]);
+            Assert.Equal(2, response[1]);
+        }
+
+        [Fact]
+        public void SolutionB_ValidInput_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new IntersectionOfTwoArraysII();
+
+            //Act
+            var response = solution.SolutionB(new[] { 1, 2, 3 }, new[] { 1, 2 });
+
+            //Assert
+            Assert.Equal(2, response.Length);
+            Assert.Equal(1, response[0]);
+            Assert.Equal(2, response[1]);
+        }
+    }
+}
