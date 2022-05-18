46beb5a2123b5197d76a729b519935329ed5ca29
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-17 22:11:58 -0300

    Add new problem: Move Zeroes

---
+++ test/Problems.Test/Array/MoveZeroesTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Array;
+using Xunit;
+
+namespace Problems.Test.Array
+{
+    public class MoveZeroesTests
+    {
+        [Theory]
+        [InlineData(new[] { 0, 1, 0, 2, 3 }, new[] { 1, 2, 3, 0, 0 })]
+        [InlineData(new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 })]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(int[] nums, int[] answer)
+        {
+            //Arrange
+            var solution = new MoveZeroes();
+
+            //Act
+            solution.Solution(nums);
+
+            //Assert
+            Assert.Equal(answer[0], nums[0]);
+            Assert.Equal(answer[1], nums[1]);
+            Assert.Equal(answer[2], nums[2]);
+        }
+    }
+}
