52b18f17a52c11ac9459825d66b0d64d72717dcc
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-17 21:22:20 -0300

    Add new problem: Plus One

---
+++ src/Problems/Array/PlusOne.cs
@@ -0,0 +1,30 @@
+﻿namespace Problems.Array
+{
+    public class PlusOne
+    {
+        public int[] Solution(int[] digits)
+        {
+            var carry = 0;
+            digits[digits.Length - 1]++;
+
+            if (digits[digits.Length - 1] < 10)
+                return digits;
+
+            var response = new int[digits.Length + 1];
+
+            for (var i = digits.Length - 1; i >= 0; i--)
+            {
+                var newCarry = (digits[i] + carry) / 10;
+                digits[i] = (digits[i] + carry) % 10;
+                response[i + 1] = digits[i];
+                carry = newCarry;
+            }
+
+            if (carry == 0)
+                return digits;
+
+            response[0] = carry;
+            return response;
+        }
+    }
+}
