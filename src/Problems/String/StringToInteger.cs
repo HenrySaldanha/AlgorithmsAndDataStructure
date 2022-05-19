97880c003ca96f0ff634430a9b50b03f133626ef
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-18 21:50:34 -0300

    Add new problem: String to Integer

---
+++ src/Problems/String/StringToInteger.cs
@@ -0,0 +1,42 @@
+﻿namespace Problems.String
+{
+    public class StringToInteger
+    {
+        public int Solution(string s)
+        {
+            var i = 0;
+            long number = 0;
+            bool positive = true;
+
+
+            while (i < s.Length && s[i] == ' ')
+                i++;
+
+            if (i < s.Length && s[i] == '-')
+            {
+                positive = false;
+                i++;
+            }
+            else if (i < s.Length && s[i] == '+')
+            {
+                positive = true;
+                i++;
+            }
+
+            while (i < s.Length && s[i] >= '0' && s[i] <= '9')
+            {
+                number *= 10;
+                number += s[i] - '0';
+
+                if (positive && number > Int32.MaxValue)
+                    return Int32.MaxValue;
+                if (!positive && number > Int32.MaxValue)
+                    return Int32.MinValue;
+
+                i++;
+            }
+
+            return positive ? (int)number : (int)number * -1;
+        }
+    }
+}
---
+++ test/Problems.Test/String/StringToIntegerTests.cs
@@ -0,0 +1,26 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class StringToIntegerTests
+    {
+        [Theory]
+        [InlineData("   -42", -42)]
+        [InlineData("+-12", 0)]
+        [InlineData("12345", 12345)]
+        [InlineData("12345123123123", int.MaxValue)]
+        [InlineData("-12345123123123", int.MinValue)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(string array, int responseExpected)
+        {
+            //Arrange
+            var solution = new StringToInteger();
+
+            //Act
+            var response = solution.Solution(array);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
