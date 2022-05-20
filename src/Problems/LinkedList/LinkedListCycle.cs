e948db7aea6f6ca52b3e5b0d3235a664b2f84f98
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-19 23:10:08 -0300

    Add new problem: Linked List Cycle

---
+++ src/Problems/LinkedList/LinkedListCycle.cs
@@ -0,0 +1,65 @@
+﻿namespace Problems.LinkedList
+{
+    /// <summary>
+    /// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+    /// There is a cycle in a linked list if there is some node in the list that can be reached 
+    /// again by continuously following the next pointer.Internally, pos is used to denote the index 
+    /// of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+    /// Return true if there is a cycle in the linked list.Otherwise, return false.
+    /// </summary>
+
+    /// <example>
+    /// Input: 
+    /// 1->2
+    /// 2->3
+    /// 3->4
+    /// 4->1
+    // , pos = 1
+    /// Output: [4,5,9]
+    /// Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+    /// </example>
+
+    public class LinkedListCycle
+    {
+        public bool SolutionA(ListNode head)
+        {
+            var visited = new HashSet<ListNode>();
+
+            while (head is not null && !visited.Contains(head))
+            {
+                visited.Add(head);
+                head = head.next;
+            }
+
+            return head != null;
+        }
+
+        public bool SolutionB(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast is not null && fast.next is not null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+    }
+}
---
+++ test/Problems.Test/LinkedList/LinkedListCycleTests.cs
@@ -0,0 +1,75 @@
+﻿using Problems.LinkedList;
+using Xunit;
+using static Problems.LinkedList.LinkedListCycle;
+
+namespace Problems.Test.LinkedList
+{
+    public class LinkedListCycleTests
+    {
+        [Fact]
+        public void SolutionA_HasCycle_MustReturnTrue()
+        {
+            //Arrange
+            var solution = new LinkedListCycle();
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = head.next;
+
+            //Act
+            var result = solution.SolutionA(head);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void SolutionA_NoCycle_MustReturnfalse()
+        {
+            //Arrange
+            var solution = new LinkedListCycle();
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+
+            //Act
+            var result = solution.SolutionA(head);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void SolutionB_HasCycle_MustReturnTrue()
+        {
+            //Arrange
+            var solution = new LinkedListCycle();
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = head.next;
+
+            //Act
+            var result = solution.SolutionB(head);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void SolutionB_NoCycle_MustReturnfalse()
+        {
+            //Arrange
+            var solution = new LinkedListCycle();
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+
+            //Act
+            var result = solution.SolutionB(head);
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
