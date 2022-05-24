692940ab32a755e66f64092c0f590832fb442c6d
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-24 20:49:24 -0300

    Add new problem: Longest Common Prefix

---
+++ src/Problems/String/LongestCommonPrefix.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.String
+{
+    /// <summary>
+    /// Write a function to find the longest common prefix string amongst an array of strings.
+    /// If there is no common prefix, return an empty string "".
+    /// </summary>
+
+    /// <example>
+    /// Input: strs = ["flower","flow","flight"]
+    /// Output: "fl"
+    /// </example>
+
+    public class LongestCommonPrefix
+    {
+        public string Solution(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+
+            var prefix = strs[0];
+            for(var i = 1; i < strs.Length; i++)
+            {
+                while (!strs[i].StartsWith(prefix))
+                    prefix = prefix.Remove(prefix.Length - 1);
+            }
+            return prefix;
+        }
+    }
+}
---
+++ test/Problems.Test/String/LongestCommonPrefixTests.cs
@@ -0,0 +1,36 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class LongestCommonPrefixTests
+    {
+        [Theory]
+        [InlineData(new[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new[] { "dog", "racecar", "car" }, "")]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(string[] input, string responseExpected)
+        {
+            //Arrange
+            var solution = new LongestCommonPrefix();
+
+            //Act
+            var response = solution.Solution(input);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+
+        [Fact]
+        public void Solution_InvalidInput_MustReturnCorrectAnswer()
+        {
+            //Arrange
+            var solution = new LongestCommonPrefix();
+
+            //Act
+            var response = solution.Solution(System.Array.Empty<string>());
+
+            //Assert
+            Assert.Equal("", response);
+        }
+    }
+}
