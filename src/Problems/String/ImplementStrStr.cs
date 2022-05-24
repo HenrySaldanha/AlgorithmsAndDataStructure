31a6869e83d6e9b1167ba87bbc1c5794ffbb3c55
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-23 23:25:13 -0300

    Add new problem: Implement StrStr

---
+++ src/Problems/String/ImplementStrStr.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.String;
+
+/// <summary>
+/// Implement strStr().
+/// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+/// or -1 if needle is not part of haystack.
+/// </summary>
+
+/// <example>
+/// Input: haystack = "hello", needle = "ll"
+/// Output: 2
+/// </example>
+
+public class ImplementStrStr
+{
+    public int Solution(string haystack, string needle)
+    {
+        for (var i = 0; i < haystack.Length; i++)
+        {
+            if (needle.Length + i > haystack.Length)
+                return -1;
+
+            if (haystack.Substring(i, needle.Length) == needle)
+                return i;
+        }
+
+        return -1;
+    }
+}
---
+++ test/Problems.Test/String/ImplementStrStrTests.cs
@@ -0,0 +1,27 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class ImplementStrStrTests
+    {
+        [Theory]
+        [InlineData("hello", "ll", 2)]
+        [InlineData("a", "a", 0)]
+        [InlineData("aaaaa", "a", 0)]
+        [InlineData("aaaaa", "bba", -1)]
+        [InlineData("abc", "c", 2)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(string a, string b, int responseExpected)
+        {
+            //Arrange
+            var solution = new ImplementStrStr();
+
+            //Act
+            var response = solution.Solution(a, b);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
+
