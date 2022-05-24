ed0e3297784bd1c8814060145280cbe8557aa750
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-05-23 23:51:08 -0300

    Add new problem: Reverse Linked List

--- src/Problems/LinkedList/ReverseLinkedList.cs
+++ src/Problems/LinkedList/ReverseLinkedList.cs
@@ -3,43 +3,34 @@
     /// <summary>
     /// Given the head of a linked list, remove the nth node from the end of the list and return its head.
     /// </summary>
-
     /// <example>
     /// Input: head = [1,2,3,4,5], n = 2
     /// Output: [1,2,3,5]
     /// </example>
-
-    /// <example>
-    /// Input: head = [1], n = 1
-    /// Output: []
-    /// </example>
-    
-    public class RemoveNthNodeFromEndOfList
+    public class ReverseLinkedList
     {
-        public ListNode Solution(ListNode head, int n)
+        public ListNode Solution(ListNode head)
         {
-            var size = 0;
-            var temp = head;
+            var curr = head;
+            ListNode last = null;
+            ListNode temp = null;
 
-            while (temp != null)
-                (size, temp) = (size + 1, temp.next);
+            while (curr is not null)
+            {
+                temp = curr.next;
+                curr.next = last;
+                last = curr;
+                curr = temp;
+            }
 
-            if (size == n)
-                return head.next;
-
-            temp = head;
-            for (var i = 0; i < size - n - 1; i++)
-                temp = temp.next;
-
-            temp.next = temp.next.next;
-
-            return head;
+            return last;
         }
 
         public class ListNode
         {
             public int val;
             public ListNode next;
+
             public ListNode(int val = 0, ListNode next = null)
             {
                 this.val = val;
