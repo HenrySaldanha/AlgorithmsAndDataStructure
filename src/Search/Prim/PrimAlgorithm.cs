d9aa8ed65b24b2ebc9dbd82cdfafcf7611fa58a2
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-06 23:26:43 -0300

    Refact Prim Algorithm

--- src/Search/Prim/PrimAlgorithm.cs
+++ src/Search/Prim/PrimAlgorithm.cs
@@ -2,55 +2,55 @@
 {
     public class PrimAlgorithm
     {
-		public int[,] GetMinimalSpanningTree(int[,] graph)
-		{
-			var graphResponse = new int[graph.GetLength(0), graph.GetLength(0)];
-			var parent = new int[graph.GetLength(0)];
-			var minimalDistances = new int[graph.GetLength(0)];
-			var visitedNodes = new bool[graph.GetLength(0)];
+        public int[,] GetMinimalSpanningTree(int[,] graph)
+        {
+            var graphResponse = new int[graph.GetLength(0), graph.GetLength(0)];
+            var parent = new int[graph.GetLength(0)];
+            var minimalDistances = new int[graph.GetLength(0)];
+            var visitedNodes = new bool[graph.GetLength(0)];
 
-			for (var i = 0; i < graph.GetLength(0); i++)
-				minimalDistances[i] = int.MaxValue;
+            for (var i = 0; i < graph.GetLength(0); i++)
+                minimalDistances[i] = int.MaxValue;
 
-			minimalDistances[0] = 0;
-			parent[0] = -1;
+            minimalDistances[0] = 0;
+            parent[0] = -1;
 
-			for (var i = 0; i < graph.GetLength(0) - 1; i++)
-			{
-				var u = SmallestEdgeIndex(minimalDistances, visitedNodes);
-				visitedNodes[u] = true;
-
-				for (var v = 0; v < graph.GetLength(0); v++)
-				{
-					if (Convert.ToBoolean(graph[u, v]) && !visitedNodes[v] && graph[u, v] < minimalDistances[v])
-					{
-						parent[v] = u;
-						minimalDistances[v] = graph[u, v];
-					}
-				}
-			}
-
-			for (var i = 1; i < graph.GetLength(0); i++)
+            for (var i = 0; i < graph.GetLength(0) - 1; i++)
             {
-				graphResponse[parent[i], i] = graph[i, parent[i]];
-				graphResponse[i, parent[i]] = graph[i, parent[i]];
-			}
-				
-			return graphResponse;
-		}
+                var u = GetSmallestEdgeIndex(minimalDistances, visitedNodes);
+                visitedNodes[u] = true;
 
-		private int SmallestEdgeIndex(int[] key, bool[] visitedNodes)
-		{
-			var min = int.MaxValue;
-			var minIndex = 0;
+                for (var v = 0; v < graph.GetLength(0); v++)
+                {
+                    if (Convert.ToBoolean(graph[u, v]) && !visitedNodes[v] && graph[u, v] < minimalDistances[v])
+                    {
+                        parent[v] = u;
+                        minimalDistances[v] = graph[u, v];
+                    }
+                }
+            }
 
-			for (var v = 0; v < visitedNodes.Length; ++v)
-			{
-				if (!visitedNodes[v] && key[v] < min)
-					(min,minIndex) = (key[v],v);
-			}
+            for (var i = 1; i < graph.GetLength(0); i++)
+            {
+                graphResponse[parent[i], i] = graph[i, parent[i]];
+                graphResponse[i, parent[i]] = graph[i, parent[i]];
+            }
 
-			return minIndex;
-		}
-	}
+            return graphResponse;
+        }
+
+        private int GetSmallestEdgeIndex(int[] key, bool[] visitedNodes)
+        {
+            var min = int.MaxValue;
+            var minIndex = 0;
+
+            for (var v = 0; v < visitedNodes.Length; ++v)
+            {
+                if (!visitedNodes[v] && key[v] < min)
+                    (min, minIndex) = (key[v], v);
+            }
+
+            return minIndex;
+        }
+    }
 }
