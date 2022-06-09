72886209bf061c9f029ed5cad610862b61be305c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-08 21:53:58 -0300

    Add new problem: Find The Difference

---
+++ test/Problems.Test/String/FindTheDifferenceTests.cs
@@ -0,0 +1,53 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class FindTheDifferenceTests
+    {
+        [Theory]
+        [InlineData("aab", "aabc", 'c')]
+        [InlineData("abcd", "dbcax", 'x')]
+        public void SolutionA_ValidInput_MustReturnCorrectAnswer(string s, string t, char responseExpected)
+        {
+            //Arrange
+            var solution = new FindTheDifference();
+
+            //Act
+            var response = solution.SolutionA(s,t);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+
+        [Theory]
+        [InlineData("aab", "aabc", 'c')]
+        [InlineData("abcd", "dbcax", 'x')]
+        public void SolutionB_ValidInput_MustReturnCorrectAnswer(string s, string t, char responseExpected)
+        {
+            //Arrange
+            var solution = new FindTheDifference();
+
+            //Act
+            var response = solution.SolutionB(s, t);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+
+        [Theory]
+        [InlineData("aab", "aabc", 'c')]
+        [InlineData("abcd", "dbcax", 'x')]
+        public void SolutionC_ValidInput_MustReturnCorrectAnswer(string s, string t, char responseExpected)
+        {
+            //Arrange
+            var solution = new FindTheDifference();
+
+            //Act
+            var response = solution.SolutionC(s, t);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
