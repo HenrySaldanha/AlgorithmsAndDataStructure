1e9ef8d4955d0cce4c8f420c6c889f021b24c748
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-09 20:00:51 -0300

    Solved new problem: Three Sum Closest

---
+++ src/Problems/Sorting/ThreeSumClosest.cs
@@ -0,0 +1,51 @@
+﻿namespace Problems.Sorting
+{
+    /// <summary>
+    /// Given an integer array nums of length n and an integer target, 
+    /// find three integers in nums such that the sum is closest to target.
+    /// Return the sum of the three integers.
+    /// You may assume that each input would have exactly one solution.
+    /// </summary>
+
+    /// <example>
+    /// Input: nums = [-1,2,1,-4], target = 1
+    /// Output: 2
+    /// </example>
+
+    /// <example>
+    /// Input: nums = [0,0,0], target = 1
+    /// Output: 0
+    /// </example>
+
+    public class ThreeSumClosest
+    {
+        public int Solution(int[] nums, int target)
+        {
+            System.Array.Sort(nums);
+            var sumTarget = nums[0] + nums[1] + nums[2];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var leftIndex = i + 1;
+                var rightIndex = nums.Length - 1;
+                while (leftIndex < rightIndex)
+                {
+                    var sum = nums[leftIndex] + nums[i] + nums[rightIndex];
+
+                    if (System.Math.Abs(sum - target) < System.Math.Abs(sumTarget - target))
+                        sumTarget = sum;
+
+                    if (sum == target)
+                        return sum;
+
+                    if (sum > target)
+                        rightIndex--;
+                    else
+                        leftIndex++;
+                }
+            }
+
+            return sumTarget;
+        }
+    }
+}
---
+++ test/Problems.Test/Sorting/ThreeSumClosestTests.cs
@@ -0,0 +1,23 @@
+﻿using Problems.Sorting;
+using Xunit;
+
+namespace Problems.Test.Sorting
+{
+    public class ThreeSumClosestTests
+    {
+        [Theory]
+        [InlineData(new[] { -1, 2, 1, -4 }, 1, 2)]
+        [InlineData(new[] { 0, 0, 0 }, 1, 0)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(int[] nums, int target, int expectedResponse)
+        {
+            //Arrange
+            var solution = new ThreeSumClosest();
+
+            //Act
+            var response = solution.Solution(nums, target);
+
+            //Assert
+            Assert.Equal(response, expectedResponse);
+        }
+    }
+}
