f2bdac633a2e02829a4e20e6b5abfc2b9f300ff7
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-14 19:47:15 -0300

    Solved new problem: Sort List

---
+++ src/Problems/Sorting/SortList.cs
@@ -0,0 +1,46 @@
+﻿namespace Sorting
+{
+    /// <summary>
+    /// Given the head of a linked list, return the list after sorting it in ascending order.
+    /// </summary>
+
+    /// <example>
+    /// Input: head = [4,2,1,3]
+    /// Output: [1,2,3,4]
+    /// </example>
+
+    public class SortList
+    {
+        public ListNode Solution(ListNode head)
+        {
+            var aux = head;
+            var list = new List<int>();
+            while (aux != null)
+            {
+                list.Add(aux.val);
+                aux = aux.next;
+            }
+            list.Sort();
+
+            aux = head;
+            for (var i = 0; i < list.Count; i++)
+            {
+                head.val = list[i];
+                head = head.next;
+            }
+            return aux;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ test/Problems.Test/Sorting/SortListTests.cs
@@ -0,0 +1,28 @@
+﻿using Sorting;
+using Xunit;
+
+namespace Problems.Test.Sorting
+{
+    public class SortListTests
+    {
+        [Fact]
+        public void Solution_ValidInput_MustReturnSuccess()
+        {
+            //Arrange
+            var solution = new SortList();
+            var head = new ListNode(4);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(1);
+            head.next.next.next = new ListNode(3);
+
+            //Act
+            var result = solution.Solution(head);
+
+            //Assert
+            Assert.Equal(1,result.val);
+            Assert.Equal(2, result.next.val);
+            Assert.Equal(3, result.next.next.val);
+            Assert.Equal(4, result.next.next.next.val);
+        }
+    }
+}
