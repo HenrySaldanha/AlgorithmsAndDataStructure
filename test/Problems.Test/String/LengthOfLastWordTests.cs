fb29a09a7a3b32647f8a84f36c01bf0fe841c099
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-14 23:31:30 -0300

    Add new problem: Length Of Last Word

---
+++ test/Problems.Test/String/LengthOfLastWordTests.cs
@@ -0,0 +1,24 @@
+﻿using Problems.String;
+using Xunit;
+
+namespace Problems.Test.String
+{
+    public class LengthOfLastWordTests
+    {
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("luffy is still joyboy", 6)]
+        public void Solution_ValidInput_MustReturnCorrectAnswer(string array, int responseExpected)
+        {
+            //Arrange
+            var solution = new LengthOfLastWord();
+
+            //Act
+            var response = solution.Solution(array);
+
+            //Assert
+            Assert.Equal(responseExpected, response);
+        }
+    }
+}
