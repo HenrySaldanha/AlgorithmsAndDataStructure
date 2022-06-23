c22f15388565b2ed12054ad7742eec452e999048
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-22 21:21:17 -0300

    Add new problem: Palindrome Number

---
+++ test/Problems.Test/Math/PalindromeNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using Problems.Math;
+
+namespace Problems.Test.Math;
+public class PalindromeNumberTests
+{
+    [Theory]
+    [InlineData(121, true)]
+    [InlineData(885588, true)]
+    [InlineData(-121, false)]
+    public void SolutionA_ValidInput_MustReturnCorrectAnswer(int input, bool answer)
+    {
+        //Arrange
+        var solution = new PalindromeNumber();
+
+        //Act
+        var response = solution.SolutionA(input);
+
+        //Assert
+        Assert.Equal(answer, response);
+    }
+}
