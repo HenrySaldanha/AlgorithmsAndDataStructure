b47eef350b3084a6fd7ce355cee065976999db4d
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-12 20:25:51 -0300

    Add new seach algorithm: Interpolation Search

---
+++ src/Search/InterpolationSearch/InterpolationSearch.cs
@@ -0,0 +1,29 @@
+﻿namespace Search.InterpolationSearch;
+public static class InterpolationSearch
+{
+    public static bool HasValue(this int[] array, int valueSearch)
+    {
+        var rightIndex = array.Length - 1;
+        var leftIndex = 0;
+
+        while (valueSearch >= array[leftIndex] && 
+               valueSearch <= array[rightIndex] && 
+               leftIndex <= rightIndex)
+        {
+            var probe = leftIndex + (rightIndex - leftIndex) * (valueSearch - array[leftIndex]) / (array[rightIndex] - array[leftIndex]);
+
+            if (rightIndex == leftIndex)
+                return array[leftIndex] == valueSearch;
+
+            if (array[probe] == valueSearch)
+                return true;
+
+            if (array[probe] < valueSearch)
+                leftIndex = probe + 1;
+            else
+                rightIndex = probe - 1;
+        }
+        return false;
+    }
+
+}
---
+++ test/Search.Test/InterpolationSearch/InterpolationSearchTests.cs
@@ -0,0 +1,38 @@
+﻿using Search.InterpolationSearch;
+
+namespace Search.Test.InterpolationSearch;
+
+public class InterpolationSearchTests
+{
+    [Theory]
+    [InlineData(-10)]
+    [InlineData(3)]
+    [InlineData(44567)]
+    public void HasValue_ValidKey_MustExists(int key)
+    {
+        //Arrange
+        var array = new int[] { -10, 1, 2, 3, 4, 5, 123, 1234, 2345, 4567, 12331, 44567 };
+
+        //Act
+        var result = array.HasValue(key);
+
+        //Assert
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(-50)]
+    [InlineData(10)]
+    [InlineData(60000)]
+    public void Exists_InvalidKey_MustNotExists(int key)
+    {
+        //Arrange
+        var array = new int[] { -10, 1, 2, 3, 4, 5, 123, 1234, 2345, 4567, 12331, 44567 };
+
+        //Act
+        var result = array.HasValue(key);
+
+        //Assert
+        Assert.False(result);
+    }
+}
